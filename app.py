08333575dca001a4f11e7e2316b510ab6759c1b5
chowhounda <chowhoundw@sina.com>
2019-12-10 14:05:42 +0800

    updated of application

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/index')
+def index():
+    return 'Hello Index'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
